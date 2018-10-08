540caa1143fff9cdc40f571a60b1f391d6dfb7e5
Iain McKenzie <iainmck29@hotmail.com>
2018-10-08 14:19:29 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -61,9 +61,9 @@ def valid_move?(position)
   end
 
   def current_player
-   if turn_count(@board) == 0
+   if turn_count == 0
      return "X"
-   elsif turn_count(@board) % 2 == 0
+   elsif turn_count % 2 == 0
      return "X"
    else return "O"
    end
